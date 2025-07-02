a7b8f28e390b24c6c8008f9574704d9923f8b37b
Elly0816 <elzoremmanuel@gmail.com>
2025-07-02 22:39:58 +0100

    Operators Operands and Expressions

--- Hello World Java/src/OperatorsOperandsAndExpressions.java
+++ Hello World Java/src/OperatorsOperandsAndExpressions.java
@@ -1,6 +1,11 @@
-class Main {
+class OperatorsOperandsAndExpressions {
 
     public static void main(String[] args) {
+        double result = 8.5;
+        result /= 1.5;
+        // result = result - 3.5;
+
+        System.out.println("result: " + result);
 
     }
 }
