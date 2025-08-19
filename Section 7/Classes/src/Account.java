a7249da2a40e3ffe50276a2cbf5e339fd8b022a6
Elly0816 <elzoremmanuel@gmail.com>
2025-08-19 22:00:27 +0100

    first car and account class

--- Section 7/Classes/src/Account.java
+++ Section 7/Classes/src/Account.java
@@ -1,105 +1,126 @@
+import java.util.regex.Pattern;
+
 public class Account {
-    public static void main(String[] args) {
 
-    }
-
-    // private properties
-    private String number;
+    private long number;
     private double balance;
     private String customerName;
     private String email;
     private String phoneNumber;
 
-    //Constructor
     /**
-     * @param Leave empty for object with default arguments
-     * @param Pass in arguments to have your own object
-     * @return Returns an account object with a number. balance. customer name, email and 
-     * phone number
+     * 
+     * @param amount amount to deposit
      */
-    public Account(){
-    this("0000", -5.00,
-    "Default Name", "default email", "Default Phone");
-        System.out.println("Empty constructor called");
+    public void depositFunds(double amount) {
+        if (amount < 0) {
+            System.out.println("Invalid amount! " + amount);
+            return;
+        }
+
+        this.balance += amount;
+        System.out.println("Added $" + amount + " to balance. Current balance is now $" + balance);
     }
-    
-    public Account(String number, double balance, String customerName,
-    String email, String phoneNumber){
-        System.out.println("Constructor with arguments called");
-        this.number = number;
-        if (balance >= 0){
-            this.balance = balance;
-        } else  {
-            this.balance = 0;
+
+    /**
+     * 
+     * @param amount amount to withdraw
+     * 
+     */
+    public void withdrawFunds(double amount) {
+        if (amount < 0) {
+            System.out.println("Invalid Input");
+            return;
+        }
+        if (amount > this.balance) {
+            System.out.println("Insufficient funds");
+            return;
+        }
+        this.balance -= amount;
+        System.out.println("Deducted $" + amount + " from balance. Current balance is now $" + balance);
+    }
+
+    /**
+     * @return the balance
+     */
+    public double getBalance() {
+        return balance;
+    }
+
+    /**
+     * @return the customerName
+     */
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @return the number
+     */
+    public long getNumber() {
+        return number;
+    }
+
+    /**
+     * @return the phoneNumber
+     */
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    /**
+     * @param balance the balance to set
+     */
+    public void setBalance(double balance) {
+        if (balance < 0)
+            return;
+        this.balance = balance;
+    }
+
+    /**
+     * @param customerName the customerName to set
+     */
+    public void setCustomerName(String customerName) {
+        if (!Pattern.compile("^[A-Za-z ]+$").matcher(customerName).matches()) {
+            System.out.println("Name should only contain letters and space!");
+            return;
         }
         this.customerName = customerName;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        if (!Pattern.compile("^[A-Za-z]+\\.?[A-Za-z]*[0-9]*@[a-z]+\\.(com|org|net)$").matcher(email).matches()) {
+            System.out.println("Invalid Email! ");
+            return;
+        }
         this.email = email;
+    }
+
+    /**
+     * @param number the number to set
+     */
+    public void setNumber(long number) {
+        this.number = number;
+    }
+
+    /**
+     * @param phoneNumber the phoneNumber to set
+     */
+    public void setPhoneNumber(String phoneNumber) {
+        if (!Pattern.compile("^\\+?[0-9]+$").matcher(phoneNumber).matches() || phoneNumber.length() > 15) {
+            System.out.println("Invalid Number! ");
+            return;
+        }
         this.phoneNumber = phoneNumber;
     }
-
-    public Account(String customerName, String email, String phoneNumber){
-        this("0000", -5.00,
-    customerName, email, phoneNumber);
-        // this.customerName = customerName;
-        // this.email = email;
-        // this.phoneNumber = phoneNumber;
-    }
-
-    // functional methods
-    public void deposit(double amount) {
-        this.balance += amount;
-        System.out.println(amount + " was successfully dpeosited, your current balance is now: $" + this.balance);
-    }
-
-    public void withdraw(double amount) {
-        if ((this.balance - amount) < 0) {
-            System.out.println("You do not have enough funds, Current balance is: $" + this.balance);
-        } else {
-            this.balance -= amount;
-            System.out.println("Withdraw Successful, your remaining balance is: $" + this.balance);
-        }
-    }
-
-    // Getters
-    public String getNumber() {
-        return this.number;
-    }
-
-    public String getBalance() {
-        return "$" + this.balance;
-    }
-
-    public String getCustomerName() {
-        return this.customerName;
-    }
-
-    public String getEmail() {
-        return this.email;
-    }
-
-    public String getPhoneNumber() {
-        return this.phoneNumber;
-    }
-
-    // Setters
-    // public void setNumber(String number) {
-    //     this.number = number;
-    // }
-
-    // public void setBalance(double balance) {
-    //     this.balance = balance;
-    // }
-
-    // public void setCustomerName(String customerName) {
-    //     this.customerName = customerName;
-    // }
-
-    // public void setEmail(String email) {
-    //     this.email = email;
-    // }
-
-    // public void setPhoneNumber(String phoneNumber) {
-    //     this.phoneNumber = phoneNumber;
-    // }
-
 }
--- Section 7/Classes/src/Car.java
+++ Section 7/Classes/src/Car.java
@@ -1,68 +1,113 @@
-
 public class Car {
-    public static void main(String[] args) {
-        // Car car = new Car();
-        // car.describeCar();
-    }
 
     private String make = "Tesla";
-    private String model = "Model X";
-    private String color = "Gray";
+    private String model = "Model-X";
+    private String colour = "Gray";
     private int doors = 2;
     private boolean convertible = true;
 
+    // public Car(String make, String model, String colour, int doors, boolean
+    // convertible) {
+    // this.make = make;
+    // this.model = model;
+    // this.colour = colour;
+    // this.doors = doors;
+    // this.convertible = convertible;
+    // }
+
+    /**
+     * 
+     * @Elly0816
+     *           Prints out a description of the car.
+     */
     public void describeCar() {
-        System.out.println(doors + "-Door " + color +
-                " " + make + " " + model + " " + (convertible ? "Convertible" : ""));
+        System.out.println(
+                doors + "-Door " + colour + " " + make + " " + model + " " + (convertible ? "convertible" : ""));
     }
 
+    /**
+     * 
+     * @return the make of the car
+     */
     public String getMake() {
         return make;
     }
 
-    public String getModel() {
-        return model;
+    /**
+     * @return the colour of the car
+     */
+    public String getColour() {
+        return colour;
     }
 
-    public String getColor() {
-        return color;
-    }
-
+    /**
+     * @return the number of doors the car has
+     */
     public int getDoors() {
         return doors;
     }
 
-    public boolean getConvertible() {
+    /**
+     * @return the model of the car
+     */
+    public String getModel() {
+        return model;
+    }
+
+    /**
+     * 
+     * @return true if the car is a convertible, false otherwise
+     */
+
+    public boolean isConvertible() {
         return convertible;
     }
 
+    /**
+     * 
+     * @param make the make to set
+     */
     public void setMake(String make) {
         if (make == null) {
-            this.make = "Unkown";
-        }
-        String lowercaseMake = make.toLowerCase();
-        switch (lowercaseMake) {
-            case "porsche", "holden", "tesla" -> this.make = make;
-            default -> {
-                this.make = "Unsupported";
+
+            this.make = "Unknown";
+        } else {
+
+            // this.make = make;
+            String lowercaseMake = make.toLowerCase();
+            switch (lowercaseMake) {
+                case "holden", "porsche", "tesla" -> this.make = make;
+                default -> this.make = "Unsupported";
             }
         }
     }
 
-    public void setModel(String model) {
-        this.model = model;
+    /**
+     * @param colour the colour to set
+     */
+    public void setColour(String colour) {
+        this.colour = colour;
     }
 
+    /**
+     * @param convertible the convertible to set
+     */
+    public void setConvertible(boolean convertible) {
+        this.convertible = convertible;
+    }
+
+    /**
+     * @param doors the doors to set
+     */
     public void setDoors(int doors) {
         this.doors = doors;
     }
 
-    public void setColor(String color) {
-        this.color = color;
-    }
-
-    public void setConvertible(boolean convertible) {
-        this.convertible = convertible;
+    /**
+     * @param model the model to set
+     */
+    public void setModel(String model) {
+        this.model = model;
     }
 
 }
