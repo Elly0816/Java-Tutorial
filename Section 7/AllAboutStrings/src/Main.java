5a4f858b7d08ccb9a272e5db2eec58c5a8694023
Elly0816 <elzoremmanuel@gmail.com>
2025-08-22 18:00:52 +0100

    string formatting and all about strings

--- Section 7/AllAboutStrings/src/Main.java
+++ Section 7/AllAboutStrings/src/Main.java
@@ -1,11 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        printInformation("Hello World!");
+        printInformation("");
     }
 
     public static void printInformation(String string) {
         int length = string.length();
         System.out.printf("Length: %,d%n", length);
+        if (string.isEmpty())
+            return;
         System.out.printf("First character: %c%n", string.charAt(0));
         System.out.printf("Last character: %c%n", string.charAt(length - 1));
 
