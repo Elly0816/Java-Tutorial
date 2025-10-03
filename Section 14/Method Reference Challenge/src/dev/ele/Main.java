3074a74bab77f0008fab606ad1becc2b656a2ed6
Elly0816 <elzoremmanuel@gmail.com>
2025-10-03 15:43:33 +0100

    method reference challenge

--- Section 14/Method Reference Challenge/src/dev/ele/Main.java
+++ Section 14/Method Reference Challenge/src/dev/ele/Main.java
@@ -12,14 +12,14 @@ public class Main {
                         "  JeRemiaH     ", "       CaleB         ", "      EmManUel           "));
 
         System.out.println("Names");
-        printNewLine(names);
+        printOnNewLines(names);
 
         operateNames(names);
 
         System.out.println("-".repeat(35));
 
         System.out.println("Transformed names");
-        printNewLine(names);
+        printOnNewLines(names);
     }
 
     private static String applyOperators(String name) {
@@ -31,12 +31,14 @@ public class Main {
     }
 
     private static void operateNames(List<String> names) {
-        for (int i = 0; i < names.size(); i++) {
-            names.set(i, names.get(i).transform(Main::applyOperators));
-        }
+        // for (int i = 0; i < names.size(); i++) {
+        // names.set(i, names.get(i).transform(Main::applyOperators));
+        // }
+
+        names.replaceAll(Main::applyOperators);
     }
 
-    private static void printNewLine(List<String> names) {
+    private static void printOnNewLines(List<String> names) {
         for (var n : names) {
             System.out.println(n);
         }
