3169805469bbd5276aed090de6094d44abd4ffe0
Elly0816 <elzoremmanuel@gmail.com>
2025-10-03 15:44:35 +0100

    method reference challenge-3

--- Section 14/Method Reference Challenge/src/dev/ele/Main.java
+++ Section 14/Method Reference Challenge/src/dev/ele/Main.java
@@ -31,10 +31,6 @@ public class Main {
     }
 
     private static void operateNames(List<String> names) {
-        // for (int i = 0; i < names.size(); i++) {
-        // names.set(i, names.get(i).transform(Main::applyOperators));
-        // }
-
         names.replaceAll(Main::applyOperators);
     }
 
